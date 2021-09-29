0ad0793c376f3450188c9c13c9d43957f15c33d0
Charles <cchester1@angelo.edu>
2021-09-29 11:50:25 -0500

    ..

--- server/server.js
+++ server/server.js
@@ -4,3 +4,25 @@ const expressHandleBars = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 8001
+
+const sequelize = require('./connection/config')
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', expressHandleBars({
+  defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(require('./routes'))
+
+app.use(express.static(path.join(__dirname, 
+  'public')))
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+  })
+})
