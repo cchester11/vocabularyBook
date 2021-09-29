401008a912e14e0b16a9207f62faa4750ec61248
Charles <cchester1@angelo.edu>
2021-09-29 10:12:06 -0500

    initializing app

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const path = require('path')
+const expressHandleBars = require('express-handlebars')
+
+const app = express()
+const PORT = process.env.PORT || 8001
