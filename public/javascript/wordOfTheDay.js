ea10107df1632e17813afe154262aed2cb5bc576
Charles <cchester1@angelo.edu>
2021-10-13 23:32:01 -0500

    last bit editions to the UX of the MVP. work on add word form post request next and populate the db

--- public/javascript/wordOfTheDay.js
+++ public/javascript/wordOfTheDay.js
@@ -1,9 +1,10 @@
 let i = 0;
+let j = 0;
 
 function changeWordOfTheDayColor () {
-  const wordOfTheDay = document.querySelector('#figure')
+  const wordOfTheDay = document.querySelector('#figure1')
 
-  let color = ["tomato", "purple", "green", "yellow"]
+  let color = ["tomato", "purple", "green", "blue"]
 
   wordOfTheDay.setAttribute('style', `color: ${color[i]}`)
 
@@ -14,4 +15,19 @@ function changeWordOfTheDayColor () {
   }
 }
 
+function changeDefinitionOfTheDayColor () {
+  const definitionOfTheDay = document.querySelector('#figure2')
+
+  let color = ["tomato", "purple", "green", "blue"]
+
+  definitionOfTheDay.setAttribute('style', `color: ${color[j]}`)
+
+  j ++
+
+  if(j > color.length - 1) {
+    j = 0
+  }
+}
+
 setInterval(changeWordOfTheDayColor, 5000)
+setInterval(changeDefinitionOfTheDayColor, 5000)
