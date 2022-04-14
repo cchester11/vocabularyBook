7a8650c7b064cbd59b6f71b363a8b80dd5a89597
Charles <cchester1@angelo.edu>
2022-04-14 14:23:00 -0500

    delete wordsParam js.

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,10 @@ const clientRoutes = require('./clientRoutes/clientRoutes')
 const apiRoutesPrefix = require('./apiRoutes/prefix')
 const apiRoutesSuffix = require('./apiRoutes/suffix')
 const apiRoutesWords = require('./apiRoutes/words')
-const apiRoutesWordsParam = require('./apiRoutes/wordsParam')
 
 router.use('/', clientRoutes)
 router.use('/api', apiRoutesPrefix)
 router.use('/api', apiRoutesSuffix)
 router.use('/api', apiRoutesWords)
-router.use('/api', apiRoutesWordsParam)
 
 module.exports = router
