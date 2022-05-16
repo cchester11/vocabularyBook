65e734046fafe8418f9e01ceb54be6c553be5f60
Charles <cchester1@angelo.edu>
2022-05-15 21:32:22 -0500

    restarting the edit word problem. All of code other than that is clean

--- public/javascript/pages.js
+++ public/javascript/pages.js
@@ -1,48 +1,3 @@
-// can use replaceChild on click on editBtn
-
-// possible problem is that preventDefault only works with Jquery
-const pages = document.querySelector('#pages')
-
-function editWord(event) {
-  event.target.style.visibility = 'hidden'
-  let target = event.target.id
-  let targetSib = event.target
-  console.log(target, targetSib)
-  // console.log('button ' + clickedBtn + ' was clicked')
-  
-  // console.log(this.window.document)
-}
-  // const response = await fetch(`/api/update/${id}`, {
-  //   method: "PUT",
-  //   body: JSON.stringify({
-  //     word,
-  //     definition
-  //   }),
-  //   headers: { "Content-Type": "application/json" }
-  // })
-
-  // if (response.ok) {
-  //     console.log('successful edit')
-  //     document.location.reload()
-  // } else {
-  //   alert(response.statusText)
-  // }
-
-
-  // can study this code for help
-  
-  //deleteButton.addEventListener('click', function(event) {
-    //   event.preventDefault() 
-    //   console.log(event)
-    
-    //   if(event.target.matches('button')) {
-    //     console.log('button was clicked')
-    //     let id = event.target.parentElement.id
-    //     console.log(id)
-    //     deleteCharacter(id)
-    //   }
-    // })
-
 // deletes word but when page reloads word still appears on the list of words on the page
 async function deleteWord(clickedId) {
 
@@ -64,7 +19,5 @@ async function deleteWord(clickedId) {
     alert(response.statusText)
   }
 }
-
-pages.addEventListener('click', editWord)
 
 
