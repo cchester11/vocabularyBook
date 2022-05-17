89a49dd38e7bfa4be19a3e2090ba6ab1477162dd
Charles <cchester1@angelo.edu>
2022-05-16 22:22:49 -0500

    having issues implementing jquery

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
+const jquery = require('jquery')
 const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 8001
 
 const sequelize = require('./config/connection')
+const { ppid } = require('process')
 
 app.set('views', path.join(__dirname, 'views'))
 app.engine('handlebars', exphbs({
@@ -17,6 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(require('./routes'))
+app.use(jquery)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
