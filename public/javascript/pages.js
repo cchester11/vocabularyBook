fccf4a32190db3d9a19702c7e02aa4bbe0415ab2
Charles <cchester1@angelo.edu>
2022-05-16 21:13:20 -0500

    ..

--- public/javascript/pages.js
+++ public/javascript/pages.js
@@ -1,3 +1,14 @@
+// edit words
+$('#wordsListDiv').on('click', 'h-5', function () {
+  let word = $(this).text().trim()
+
+  let TextArea = $('<textarea>').addClass('form-control').val(word)
+
+  $(this).replaceWith(TextArea)
+
+  TextArea.trigger('focus')
+})
+
 // deletes word but when page reloads word still appears on the list of words on the page
 async function deleteWord(clickedId) {
 
