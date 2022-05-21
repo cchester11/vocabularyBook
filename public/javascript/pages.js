2c0bdc1e99cb8d3077e8c988fb2a7ade5a094e32
Charles <cchester1@angelo.edu>
2022-05-21 12:26:04 -0500

    working on editing words

--- public/javascript/pages.js
+++ public/javascript/pages.js
@@ -1,4 +1,28 @@
 // deletes word but when page reloads word still appears on the list of words on the page
+// edit words
+$('#wordListDiv').on('click', 'h-5', () => {
+  let text = $(this).text().trim()
+
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+})
+
+$("#wordsListDiv").on("change", "textarea", function () {
+  // get current value of textarea
+  var text = $(this).val();
+  
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with new content
+  $(this).replaceWith(taskP);
+});
+
 async function deleteWord(clickedId) {
 
   let current = clickedId;
