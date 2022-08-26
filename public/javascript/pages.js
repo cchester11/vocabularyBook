16bc638538bf9dd026cefdec20a6b079ea67a0b1
Charles <cchester1@angelo.edu>
2022-08-26 00:15:42 -0500

    #2 the target id is read

--- public/javascript/pages.js
+++ public/javascript/pages.js
@@ -1,6 +1,33 @@
-$("button").on('click', () => {
-  alert('button was clicked')
+$(".card-body").on('click', '.editBtn', (event) => {
+  const clickedId = event.target.id
+  console.log(clickedId)
+  // edit the field
+  
+  // call the editWord function and pass id, updated word, updated def
+  // editWord(clickedId, word, definition)
+
+  return
 })
+
+async function editWord(clickedId, word, definition) {
+  let current = clickedId
+  
+  const response = await fetch(`api/update/${current}`, {
+    method: "update",
+    body: {
+      id: current,
+      word: word,
+      definition: definition
+    },
+    headers: { 'Content-Type': 'application/json'}
+  })
+
+  if(response.ok) {
+    return response.json()
+  } else {
+    alert(response.statusText)
+  }
+}
 
 async function deleteWord(clickedId) {
 
