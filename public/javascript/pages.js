eb0a89525ff3f1fc0933a813a164607221fdfea9
Charles <cchester1@angelo.edu>
2022-08-25 21:10:36 -0500

    starting work on editing words. will start small and build up

--- public/javascript/pages.js
+++ public/javascript/pages.js
@@ -1,32 +1,4 @@
-// deletes word but when page reloads word still appears on the list of words on the page
-// edit words
 
-// use onclick function here
-// pass the id of the edit button here in the onclick
-// convert p tag into input bar
-// save changes and send in put request using stashed id
-$('#wordListDiv').on('click', 'h-5', () => {
-  let text = $(this).text().trim()
-
-  var textInput = $("<textarea>").addClass("form-control").val(text);
-  $(this).replaceWith(textInput);
-
-  // auto focus new element
-  textInput.trigger("focus");
-})
-
-$("#wordsListDiv").on("change", "textarea", function () {
-  // get current value of textarea
-  var text = $(this).val();
-  
-  // recreate p element
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(text);
-
-  // replace textarea with new content
-  $(this).replaceWith(taskP);
-});
 
 async function deleteWord(clickedId) {
 
