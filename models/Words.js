d318d7dfda0d5ed7106c90ea4751ee92e4279672
Charles <cchester1@angelo.edu>
2022-11-15 16:07:41 -0600

    #11 user_id foreign key associations commented out until further use. home btn on main.handlebars route changed

--- models/Words.js
+++ models/Words.js
@@ -22,13 +22,13 @@ Words.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'users',
-        key: 'id'
-      }
-    }
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'users',
+    //     key: 'id'
+    //   }
+    // }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const Prefix = require('./Prefix')
 const Suffix = require('./Suffix')
 
 // create associations
-Users.hasMany(Words, {
-      foreignKey: 'user_id'
-})
+// Users.hasMany(Words, {
+//       foreignKey: 'user_id'
+// })
 
-Words.belongsTo(Users, {
-      foreignKey: 'user_id'
-})
+// Words.belongsTo(Users, {
+//       foreignKey: 'user_id'
+// })
 
 // send models to routes, server, etc with their associations
 module.exports = {Users, Words, Prefix, Suffix};
