950d7ad3b57cd26d0dccf9363de03f0e9e123ad6
Charles <cchester1@angelo.edu>
2022-11-17 11:48:59 -0600

    #14 updating seed files

--- seeds/words.js
+++ seeds/words.js
@@ -26,6 +26,41 @@ const words = [
     word: 'Quintessential',
     definition: 'A typical example of something',
     user_id: 2
+  },
+  {
+    word: 'Plethora',
+    definition: 'an abundance or excessive amount of',
+    user_id: 2
+  },
+  {
+    word: 'Invariable',
+    definition: 'Not changing or subject to change',
+    user_id: 1
+  },
+  {
+    word: 'Inadvertent',
+    definition: 'unintentional consequences resulting from lack of care',
+    user_id: 1
+  },
+  {
+    word: 'Primordial',
+    definition: 'being the first in sequence',
+    user_id: 2
+  },
+  {
+    word: 'Pandemonium',
+    definition: 'a condition of noisy confusion',
+    user_id: 2
+  },
+  {
+    word: 'Adobe',
+    definition: 'Building material composed of sun dried clay and dung compacted together',
+    user_id: 1
+  },
+  {
+    word: 'Gregarious',
+    definition: 'seeking and enjoying the company of others',
+    user_id: 1
   }
 ]
 
