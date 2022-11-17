8943b0e133b6b39a650a590c312a7cd7934bed2b
Charles <cchester1@angelo.edu>
2022-11-17 10:05:52 -0600

    #14 removed login and signup routes from clientRoutes

--- routes/clientRoutes/clientRoutes.js
+++ routes/clientRoutes/clientRoutes.js
@@ -1,60 +1,7 @@
 const router = require('express').Router()
 const sequelize = require('sequelize')
 const Op = sequelize.Op
-const { Words, Users } = require('../../models')
-
-// login page route
-router.get('/', (req, res) => {
-  res.render('login')
-})
-
-// take to homepage after successful sign up
-router.post('/signupform', (req, res) => {
-  // send body {username: 'username', password: 'password'} to sequelize to create new user
-  Users.create({
-    username: req.body.username,
-    password: req.body.password
-  })
-  .then(results => {
-    req.session.save(() => {
-      // save the below tokens to the users session while the user is logged in to allow the user access to all web apps pages
-      req.session.user_id = results.id,
-      req.session.username = results.username,
-      req.session.loggedIn = true
-
-      res.json(results)
-    })
-    .catch(err => {
-      window.alert(err)
-      res.status(500).json(err)
-    })
-  })
-})
-
-router.post('/loginform', (req, res) => {
-  Users.findOne({
-    where: {
-      username: req.body.username,
-      password: req.body.password
-    }
-  })
-  .then(user => {
-    if(!user) {
-      res.status(400).json({ message: 'Wrong username or password.' })
-    }
-
-    req.session.save(() => {
-      req.session.user_id = user.id,
-      req.session.username = user.username,
-      req.session.loggedIn = true
-
-      res.json({ 
-        user: user, 
-        message: 'You are now logged in'
-      })
-    })
-  })
-})
+const { Words } = require('../../models')
 
 // home route
 router.get('/home', (req, res) => {
