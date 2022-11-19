4df583ca2f2500a6eec6f5cc90220a54cfe7b738
Charles <cchester1@angelo.edu>
2022-11-19 16:04:08 -0600

    #12 searchbar dom file started. search bar input and submit button written. next, take value from searchbar at submit and send in request to fetch for specific word. will probably need to write the route manually

---
+++ public/javascript/searchBar.js
@@ -0,0 +1,9 @@
+const searchBar = document.getElementById('nav-search-bar');
+const searchBarBtn = document.getElementById('nav-search-bar-btn');
+
+function searchBarHandler () {
+      const word = searchBar.value.trim()
+}
+
+searchBarBtn.addEventListener('click', searchBarHandler)
+
