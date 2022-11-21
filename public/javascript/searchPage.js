f4caba9c4fe44f3e9c8a71f8f203177494cfd8c3
Charles <cchester1@angelo.edu>
2022-11-21 11:36:49 -0600

    #12 added dom handler for the 'add to my words' feature. may be an issue with creating duplicate words. either way, looks like theres a problem with the fetch request

---
+++ public/javascript/searchPage.js
@@ -0,0 +1,27 @@
+const add_to_my_words_button = document.getElementById('add-to-my-words-button');
+const word = document.querySelector('.word').value
+const definition = document.querySelector('.definition').value
+
+async function handleAddToMyWordsFeature (event) {
+      event.preventDefault()
+      console.log(word, definition)
+      console.log('clicked')
+
+      const response = await fetch('/api/words', {
+            method: 'post',
+            body: JSON.stringify({
+                  word: word,
+                  definition: definition
+            }),
+            headers: { 'Content-Type': 'application/json' }
+      })
+
+      if(response.ok) {
+            window.alert("You've added a new word to your database!")
+            document.location.replace('/dictionary')
+      } else {
+            alert(response.statusText)
+      }
+}
+
+add_to_my_words_button.addEventListener('click', handleAddToMyWordsFeature)
