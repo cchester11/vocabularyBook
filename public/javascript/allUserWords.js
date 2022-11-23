487ba6cac28558fc71be91c6876ee2df85ed1b21
Charles <cchester1@angelo.edu>
2022-11-23 14:54:25 -0600

    #17 alluserwords page renders too many words. need to contain the overflow within the page somehow

---
+++ public/javascript/allUserWords.js
@@ -0,0 +1,40 @@
+const wordsListDiv = document.getElementById('wordsListDiv');
+let items = {};
+
+async function getAllUserWords() {
+      await await fetch('/api/words', {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+      })
+            .then(results => {
+                  return results.json()
+            })
+            .then(data => {
+                  items = data;
+                  console.log(items)
+            })
+            .catch(err => {
+                  window.alert('Error message ' + err)
+            })
+
+      for (let i = 0; i < items.length; i++) {
+            const betaDiv = document.createElement('div')
+            const wordEl = document.createElement('h5')
+            const definitionEl = document.createElement('p')
+
+            betaDiv.setAttribute('class', 'card-body col-12 border border-warning rounded bg-success h-5 m-auto')
+            wordEl.setAttribute('class', 'card-title font-weight-light word')
+            definitionEl.setAttribute('class', 'card-text definition')
+
+            wordEl.textContent = items[i].word
+            definitionEl.textContent = items[i].definition
+
+            betaDiv.appendChild(wordEl)
+            betaDiv.appendChild(definitionEl)
+
+            wordsListDiv.appendChild(betaDiv)
+      }
+
+}
+
+getAllUserWords()
