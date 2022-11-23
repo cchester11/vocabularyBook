8e20e44794a25c5af431f047050e8db05d36cd29
Charles <cchester1@angelo.edu>
2022-11-23 12:20:24 -0600

    #17 removed suffix and prefix routes from index.js of routes folder. was affecting the build

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
 const router = require('express').Router()
 
 const clientRoutes = require('./clientRoutes/clientRoutes')
-const apiRoutesPrefix = require('./apiRoutes/prefix')
-const apiRoutesSuffix = require('./apiRoutes/suffix')
 const apiRoutesUsers = require('./apiRoutes/users')
 const apiRoutesWords = require('./apiRoutes/words')
 const apiRoutesLikes = require('./apiRoutes/likes')
 
 router.use('/', clientRoutes)
-router.use('/api', apiRoutesPrefix)
-router.use('/api', apiRoutesSuffix)
 router.use('/api', apiRoutesUsers)
 router.use('/api', apiRoutesWords)
 router.use('/api', apiRoutesLikes)
