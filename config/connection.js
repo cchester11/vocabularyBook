4887174128ff012b3362412b5241a7e86142c16b
Charles <cchester1@angelo.edu>
2023-04-22 00:42:55 -0500

    #railway testing sequelize connection to railway deployment

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,18 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+if (process.env.PORT === 8001) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,  process.env.DB_PW, {
+    host: 'containers-us-west-71.railway.app',
+    dialect: 'mysql',
+    port: 5577,
+  })
 }
 
 module.exports = sequelize;
