871b899012dc7d88240faf915bd8c5c191ad8706
Charles <cchester1@angelo.edu>
2023-04-28 13:47:44 -0500

    env variables and connection file

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,17 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.PORT === 8001) {
+if (PORT === 8001) {
   sequelize = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
+} else {
+  sequelize = new Sequelize(process.env.GOOGLE_DB_NAME, {
+    host: process.env.GOOGLE_HOST,
+    port: 3306
+  })
 }
 
 module.exports = sequelize;
