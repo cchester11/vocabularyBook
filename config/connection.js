697a73afc376e5c42ac279f495adeec07e7d6973
Charles <cchester1@angelo.edu>
2023-04-28 12:17:34 -0500

    env variables

--- config/connection.js
+++ config/connection.js
@@ -5,18 +5,11 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.PORT === 8001) {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,  process.env.DB_PW, {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    dialect: 'mysql',
-    logging: false
-  })
 }
 
 module.exports = sequelize;
