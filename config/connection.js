d992c9f53cee4e856c8652a8624494a5ee2f7d94
Charles <cchester1@angelo.edu>
2023-05-02 19:47:12 -0500

    ..

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,11 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.host === 'localhost') {
-  sequelize = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-} else {
-  sequelize = new Sequelize(process.env.GOOGLE_DB_NAME, process.env.RAILWAY_USERNAME, process.env.RAILWAY_PASSWORD, {
+let sequelize = new Sequelize(process.env.GOOGLE_DB_NAME, process.env.RAILWAY_USERNAME, process.env.RAILWAY_PASSWORD, {
     host: process.env.GOOGLE_HOST,
     dialect: 'mysql',
     port: 3306
   })
-}
+
 
 module.exports = sequelize;
