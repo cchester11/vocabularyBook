3c6ccd43394685bec67256984201258e3ec1c5e1
Charles <cchester1@angelo.edu>
2023-05-03 23:20:19 -0500

    #railway | writing local connection code back into codebase

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,21 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize = new Sequelize(process.env.GOOGLE_DB_NAME, process.env.RAILWAY_USERNAME, process.env.RAILWAY_PASSWORD, {
+let sequelize;
+
+if (process.env.PORT = 4001) {
+  sequelize = new Sequelize(process.env.GOOGLE_DB_NAME, process.env.RAILWAY_USERNAME, process.env.RAILWAY_PASSWORD, {
     host: process.env.GOOGLE_HOST,
     dialect: 'mysql',
     port: 3306
   })
+} else {
+  sequelize = new Sequelize(process.env.DB_USER, process.env.DB_PW, process.env.DB_NAME, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  })
+};
 
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 //connection declarations
 const app = express()
 const PORT = process.env.PORT || 8001
-const IP = process.env.IP
+const IP = process.env.IP || 'localhost'
 
 // establish connection to the db
 const sequelize = require('./config/connection')
